a454ca3651ee00b3c83611177911295f6617d6cd
Rohan Kumar Chaudhary <rohankumarchaudhary67@gmail.com>
2025-01-21 00:37:21 +0530

    update: fetch usage API integrate

--- apps/web/src/app/usage/page.tsx
+++ apps/web/src/app/usage/page.tsx
@@ -1,10 +1,13 @@
 import UsageComp from "@/components/usage";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
 
 export default async function UsagePage() {
+    const session = await getServerSession(authOptions);
     return (
         <>
             <div className="paddings">
-                <UsageComp />
+                <UsageComp session={session} />
             </div>
         </>
     )
--- apps/web/src/components/usage.tsx
+++ apps/web/src/components/usage.tsx
@@ -1,12 +1,41 @@
+'use client'
 import { Progress } from "@/components/ui/progress"
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
+import { useEffect, useState } from "react"
+import axios from "axios"
 
-export default function UsageComp() {
+export default function UsageComp({ session }: { session: any }) {
+
+    const [usage, setUsage] = useState<any>();
+    const [plan, setPlan] = useState<any>();
+
+    const fetchUsage = async () => {
+        const res = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/usage/fetch`, {
+            withCredentials: true,
+            headers: {
+                Authorization: `Bearer ${session.accessToken}`,
+            },
+        });
+        setUsage(res.data.data);
+        console.log(res.data.data);
+        if(res.data.data.currentPlan === "freeTrial") {
+            setPlan("Free Trial (Basic Features)");
+        } else if(res.data.data.currentPlan === "premiumPlan") {
+            setPlan("Premium Plan (Enhanced AI Assistant)");
+        } else if(res.data.data.currentPlan === "unlimitedPremium") {
+            setPlan("Unlimited Premium Plan (All Features)");
+        }
+    }
+
+    useEffect(() => {
+        fetchUsage();
+    }, []);
+
     return (
         <>
             <div className="flex flex-col items-start justify-center gap-2 py-10">
-                <h1 className="text-xl md:text-2xl font-semibold">Free Trial (Basic Features)</h1>
+                <h1 className="text-xl md:text-2xl font-semibold">{plan}</h1>
                 <p>Your usage details and limits are displayed here.</p>
                 <div className="flex flex-col gap-6 py-6 w-full md:pl-4">
                     <div className="flex items-start justify-between gap-2 w-full">
@@ -14,9 +43,9 @@ export default function UsageComp() {
                             Open/View Issues Limit
                         </span>
                         <div className="flex flex-col items-start justify-start gap-2 w-full">
-                            <span>123/400</span>
+                            <span>{usage?.currentOpenIssue}/{usage?.openIssueLimit}</span>
                             <Progress
-                                value={30}
+                                value={(usage?.currentOpenIssue / usage?.openIssueLimit) * 100}
                                 className="h-2 w-full"
                             />
                         </div>
@@ -27,9 +56,9 @@ export default function UsageComp() {
                             AI Question Limit
                         </span>
                         <div className="flex flex-col items-start justify-start gap-2 w-full">
-                            <span className="text-blue-400">20/20</span>
+                            <span className="text-blue-400">{usage?.currentAiQuestion}/{usage?.aiQuestionLimit}</span>
                             <Progress
-                                value={100}
+                                value={(usage?.currentAiQuestion / usage?.aiQuestionLimit) * 100}
                                 className="h-2 w-full"
                             />
                             <span className="text-red-400">Upgrade plan to increase limit.</span>
@@ -37,7 +66,7 @@ export default function UsageComp() {
                     </div>
                 </div>
                 <div className="w-full flex justify-end pt-4">
-                <Link href={"/upgrade"}><Button>Upgrade Plan</Button></Link>
+                    <Link href={"/upgrade"}><Button>Upgrade Plan</Button></Link>
                 </div>
             </div>
         </>
