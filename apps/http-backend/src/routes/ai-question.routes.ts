2f3a7b494e26c8c72aae446a65cefe09508c8db0
Rohan Kumar Chaudhary <rohankumarchaudhary67@gmail.com>
2025-01-21 01:11:42 +0530

    remove: AI question backend

--- apps/http-backend/src/routes/ai-question.routes.ts
+++ apps/http-backend/src/routes/ai-question.routes.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { askAiQuestionWithIssue, askAiQuestionWithoutIssue } from "../controllers/ai-question.controller";
+import { askAiQuestionWithIssue } from "../controllers/ai-question.controller";
 import { verifySessionToken } from "../middlewares/auth-verification";
 
 const AIrouter: Router = Router();
 
 AIrouter.post("/askWithIssue", verifySessionToken, askAiQuestionWithIssue);
-AIrouter.post("/askWithoutIssue", verifySessionToken, askAiQuestionWithoutIssue);
 
 export default AIrouter;
