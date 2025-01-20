8a76c8a15101f8d3307814284e8cc82ff0c4a293
Rohan Kumar Chaudhary <rohankumarchaudhary67@gmail.com>
2025-01-21 00:46:54 +0530

    update: fetch bookmark API integrate

--- apps/web/src/app/bookmarks/page.tsx
+++ apps/web/src/app/bookmarks/page.tsx
@@ -1,10 +1,13 @@
 import BookmarkComp from "@/components/bookmark";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
 
-export default function BookmarksPage() {
+export default async function BookmarksPage() {
+    const session = await getServerSession(authOptions);
     return (
         <>
             <div className="paddings">
-                <BookmarkComp />
+                <BookmarkComp session={session} />
             </div>
         </>
     )
--- apps/web/src/components/bookmark.tsx
+++ apps/web/src/components/bookmark.tsx
@@ -1,20 +1,35 @@
+'use client'
 import IssueCard from "./issues/issue-card";
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import IssueType from "@/types/issue-type";
 
-export default function BookmarkComp() {
+export default function BookmarkComp({session}: {session: any}) {
+
+    const [issues, setIssues] = useState<IssueType[]>([]);
+
+    const fetchBookmarks = async () => {
+        const res = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/bookmark/fetchAll`, {
+            withCredentials: true,
+            headers: {
+                Authorization: `Bearer ${session.accessToken}`,
+            },
+        });
+        setIssues(res.data.data.issues);
+    }
+
+    useEffect(() => {
+        fetchBookmarks();
+    }, []);
+
     return (
         <>
             <div className="flex flex-col items-start justify-center gap-2 py-6">
                 <h1 className="text-xl md:text-2xl font-semibold">Bookmarked Issues</h1>
-                <div className="w-full grid-cols-2 gap-4 md:grid pt-2"> 
-                    <IssueCard />
-                    <IssueCard />
-                    <IssueCard />
-                    <IssueCard />
-                    <IssueCard />
-                    <IssueCard />
-                    <IssueCard />
-                    <IssueCard />
-                    <IssueCard />
+                <div className="w-full grid md:grid-cols-2 gap-4 md:grid pt-2"> 
+                    {issues.map((issue, index)=>{
+                        return <IssueCard key={index} issue={issue} />
+                    })}
                 </div>
             </div>
         </>
