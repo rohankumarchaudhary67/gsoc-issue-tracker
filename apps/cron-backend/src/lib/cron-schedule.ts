1f04ce9c5d573eebb4687735a8b7e92773652a41
Rohan Kumar Chaudhary <rohankumarchaudhary67@gmail.com>
2025-02-01 19:49:44 +0530

    update: Cron backend

--- apps/cron-backend/src/lib/cron-schedule.ts
+++ apps/cron-backend/src/lib/cron-schedule.ts
@@ -1,24 +1,14 @@
 import cron from 'node-cron';
 import fetchIssues from './fetch-issues';
-import repoList from '../repo-list';
 
-const cronFetchSchedule = {
+const cronSchedule = {
     start: (): void => {
-        let index = 0;
         cron.schedule('0 * * * *', () => {
-            const currentRepo = repoList[index];
-            console.log('Fetching issues from GitHub for ', currentRepo);
-            fetchIssues(currentRepo)
-                .then(() =>
-                    console.log('Issues fetched and stored of ', currentRepo)
-                )
-                .catch((err: unknown) =>
-                    console.error('Error fetching issues:', err)
-                );
-
-            index = (index + 1) % repoList.length;
+            console.log('Fetching issues from GitHub...');
+            fetchIssues()
+                .then(() => console.log('Issues fetched and stored'))
+                .catch((err: unknown) => console.error('Error fetching issues:', err));
         });
     },
 };
-
-export default cronFetchSchedule;
+export default cronSchedule;
--- apps/cron-backend/src/lib/fetch-issues.ts
+++ apps/cron-backend/src/lib/fetch-issues.ts
@@ -1,61 +1,56 @@
 import axios from 'axios';
+import repoList from '../repo-list';
 import prisma from '@repo/db/db';
-
 interface IssueProps {
-    repository: string;
-    html_url: string;
-    number: number;
-    state: string;
-    title: string;
-    comments: number;
-    labels: string[];
-    created_at: string;
-    updated_at: string;
+    repository: string
+    html_url: string,
+    number: number,
+    state: string,
+    title: string,
+    body: string,
+    comments: number,
+    labels: string[],
+    created_at: string,
+    updated_at: string
 }
-
-const fetchIssues = async (currentRepo: string | undefined): Promise<void> => {
+const fetchIssues = async (): Promise<void> => {
     const githubToken = process.env.GITHUB_TOKEN;
-
     if (!githubToken) {
-        throw new Error('Github token is missing.');
+        throw new Error('Github token is missing.')
     }
-
-    const url = `https://api.github.com/repos/${currentRepo}/issues?assignee=none`;
-
-    try {
-        const response = await axios.get(url, {
-            headers: {
-                Authorization: `token ${githubToken}`,
-                'User-Agent': 'GSoC-Issue-Tracker',
-            },
-        });
-
-        const issues = response.data.map((issue: IssueProps) => ({
-            repository: currentRepo,
-            url: issue.html_url,
-            number: issue.number,
-            state: issue.state,
-            title: issue.title,
-            comments: issue.comments,
-            labels: issue.labels.map((label: any) => label.name),
-            createdAt: issue.created_at,
-            updatedAt: issue.updated_at,
-        }));
-
-        await prisma.issue.deleteMany({
-            where: {
-                repository: currentRepo,
-            },
-        });
-
-        if (issues.length > 0) {
-            await prisma.issue.createMany({
-                data: issues,
+    for (const repo of repoList) {
+        const url = `https://api.github.com/repos/${repo}/issues?assignee=none`;
+        try {
+            const response = await axios.get(url, {
+                headers: {
+                    Authorization: `token ${githubToken}`,
+                    'User-Agent': 'GSoC-Issue-Tracker',
+                },
+            })
+            const issues = response.data.map((issue: IssueProps) => ({
+                repository: repo,
+                url: issue.html_url,
+                number: issue.number,
+                state: issue.state,
+                title: issue.title,
+                comments: issue.comments,
+                labels: issue.labels.map((label: any) => label.name),
+                createdAt: issue.created_at,
+                updatedAt: issue.updated_at
+            }))
+            await prisma.issue.deleteMany({
+                where: {
+                    repository: repo,
+                },
             });
+            if (issues.length > 0) {
+                await prisma.issue.createMany({
+                    data: issues,
+                });
+            }
+        } catch (error: any) {
+            console.log(`Error fetch issue for ${repo}`)
         }
-    } catch (error: any) {
-        console.log(`Error fetch issue for ${currentRepo}`);
     }
-};
-
+}
 export default fetchIssues;
